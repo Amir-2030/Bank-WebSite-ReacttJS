583b63e1d2017e394ee244b844d9fef4c49076b7
Mohamed Amir <84458105+Amir-2030@users.noreply.github.com>
2022-02-27 03:21:42 +0200

    update

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
 <header>
   <h1><img src="images/Logo.png"alt="sorry" className="logoImg"/> </h1>
-  <button className="navBtn"><a href="/">الرئيسية</a></button>
+  <button className="navBtn"><a href="/" target="_blank">الرئيسية</a></button>
     <button className="navBtn"><a href="/Services">خدمتنا</a></button>
      <button className="navBtn"><a href="/OurCustomer">عملائنا </a></button>
     <button className="navBtn"><a href="/JoinUs">أنضم الينا </a></button>
     <button className="navBtn"><a href="/ContactUs">تواصل معنا </a></button>
     <button  className="navBtn  Loginbutoon"><a href="/Login">تسجيل دخول</a></button>
-    <button className="navBtn Loginbutoon"><a hyyref="/SignUp">انشاء حساب جديد </a></button>
+    <button className="navBtn Loginbutoon"><a href="/SignUp">انشاء حساب جديد </a></button>
 </header>
   );
 }
